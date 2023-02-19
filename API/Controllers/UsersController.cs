91c059cfdf316a824a0df3a08f79dd7a37cb7656
jan <janlamza12@gmail.com>
2023-02-19 18:54:56 +0100

    Extending the API Entity Framework relationship Seeding data Repository pattern DTO's Adding AutoMapper AutoMapper Optimising SQL Query with AutMap

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,28 +18,29 @@ namespace API.Controllers
     public class UsersController : BaseApiController
     {
 
-        private readonly DataContext _context;
 
-        public UsersController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
+
         }
 
-        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-
-            var users = await _context.Users.ToListAsync();
-            return users;
+            var users = await _userRepository.GetMembersAsync();
+            return Ok(users);
         }
 
-    
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
-        {
 
-            return await _context.Users.FindAsync(id);
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            return await _userRepository.GetMemberAsync(username);
         }
     }
 }
---
+++ API/Data/Seed.cs
@@ -0,0 +1,34 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class Seed
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if ( await context.Users.AnyAsync()) return;
+
+            var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+
+            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+
+            var Users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+            foreach(var user in Users){
+                using var hmac = new HMACSHA512();
+
+                user.UserName =  user.UserName.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
+                user.PasswordSalt = hmac.Key;
+
+                context.Users.Add(user);
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
