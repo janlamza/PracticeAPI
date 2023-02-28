a1f4049cc8b88a71090862c0cbe701b66b140744
jan <janlamza12@gmail.com>
2023-02-28 21:14:53 +0100

    Added LikeUser feature (many-to-many relationship) Added Entity Added interface Added Repository Added Controller

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -21,6 +21,7 @@ namespace API.Extensions
 
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IPhotoService,PhotoService>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
